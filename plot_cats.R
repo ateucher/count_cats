31c5eeb40d6208a483b643acc69e08b3d3fcfb86
ateucher <Andy.Teucher@gmail.com>
2015-04-01 17:40:29 -0700

    Reorder plot elements, change font sizes

--- plot_cats.R
+++ plot_cats.R
@@ -16,13 +16,13 @@ summary_plot <- ggplot(data,
              labeller = label_wrap_gen(15)) +
   geom_point(size = 2, colour = "#377eb8") +
   geom_vline(aes(xintercept = 55), linetype = 2, colour = "#e41a1c") +
-  labs(title="Number of cats at different cat monitoring sites in B.C.",
-       x = "Number of cats", y = "Cat Monitoring Station") +
   geom_text(data = ann_text, label = "Maximum regulated\nnumber of cats (55)", size = 3,
             hjust = 0, colour = "#e41a1c") +
+  labs(title="Number of cats at different cat monitoring sites in B.C.",
+       x = "Number of cats", y = "Cat Monitoring Station") +
   theme_bw() +
   theme(axis.title = element_text(size=rel(1)),
-        axis.text.y = element_text(size = rel(0.7)),
+        axis.text.y = element_text(size = rel(0.8)),
         axis.line.x = element_blank(),
         strip.text = element_text(size = rel(0.75)))
 
